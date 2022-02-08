12c7a931cfb6f511450539686b9a5aa444656359
David Munoz <david.muv91@gmail.com>
2022-02-08 02:37:04 -0600

    Backup automation

---
+++ backup/backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+VOL_NAME=p_backup
+DESTINATION=/run/media/${USER}/${VOL_NAME}/$(date +%d-%B-%Y)
+EXCLUDE_FILE="backup_ignore"
+
+rsync --archive --verbose --human-readable --progress ${HOME}/Documents ${DESTINATION} --exclude-from ${EXCLUDE_FILE}
+rsync --archive --verbose --human-readable --progress ${HOME}/Music ${DESTINATION} --exclude-from ${EXCLUDE_FILE}
+rsync --archive --verbose --human-readable --progress ${HOME}/Pictures ${DESTINATION} --exclude-from ${EXCLUDE_FILE}
+rsync --archive --verbose --human-readable --progress ${HOME}/Downloads ${DESTINATION} --exclude-from ${EXCLUDE_FILE}
+
+rsync --archive --verbose --human-readable --progress ${HOME}/data ${DESTINATION} --exclude-from ${EXCLUDE_FILE}
+rsync --archive --verbose --human-readable --progress ${HOME}/._values ${DESTINATION}/_values --exclude-from ${EXCLUDE_FILE}
