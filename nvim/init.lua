f8f8942ac3b7b45e8f964f805cd0284bbdb0ed40
David Munoz <david.muv91@gmail.com>
2025-07-28 16:25:11 -0600

    neovim line options

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,5 +7,7 @@ vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 
